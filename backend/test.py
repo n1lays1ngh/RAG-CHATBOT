55278847eef45707c6eaa5862d658c78c28ed1c2
Nilay Singh <65809374+n1lays1ngh@users.noreply.github.com>
2026-01-03 16:33:38 +0530

    Update sample documents in test.py

--- backend/test.py
+++ backend/test.py
@@ -3,10 +3,10 @@ from utils.retriever import save_to_faiss, retrieve_chunks
 
 # Sample documents to ingest
 docs = [
-    "Aadya Singh has a height of 160cm and is 62kg",
-    "Aadya Singh has a brother called nilay who is the prime minister of india ",
-    "Nilay has an electric guitar called fender squire and an amp called marshall",
-    "aadya has a macbook air m4 "
+    "AMit Trivedi has a height of 160cm and is 62kg",
+    "AMit Trivedi has a brother called Sambhu  who is the prime minister of india ",
+    "Sambhu has an electric guitar called fender squire and an amp called marshall",
+    "Amit has a macbook air m4 "
 ]
 
 # Step 1: Embed and save
